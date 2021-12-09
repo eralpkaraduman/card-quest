b23d0c1c2a6b762ebbfe8626cb756fde6e92dda6
Eralp Karaduman <eralp@eralpkaraduman.com>
2021-12-09 16:43:02 +0200

    Add react-navigation

---
+++ article/article.md
@@ -0,0 +1,5 @@
+# React Native Web
+
+- Common way is to make separate react-dom, react-native projects in a monorepo
+- This approach explores the idea adding react-dom into a react-native project in a single project
+  - No need to symlink files/folders 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,74 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {View, StatusBar, StyleSheet, Text} from 'react-native';
 import {colors} from './colors';
 import {Counter} from '@components/Counter';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Icon} from 'react-native-vector-icons/Icon';
+
+const Tab = createBottomTabNavigator();
+const TabBarIcons: {
+  [key in string]: {
+    component: typeof Icon;
+    active: string;
+    inactive: string;
+  };
+} = {
+  HomeTab: {
+    component: Ionicons,
+    active: 'ios-information-circle',
+    inactive: 'ios-information-circle-outline',
+  },
+};
+function getTabBarIcon(
+  routeName: string,
+  focused: boolean,
+  color: string,
+  size: number,
+): React.ReactElement {
+  const {component, active, inactive} = TabBarIcons[routeName];
+  return React.createElement(component, {
+    name: focused ? active : inactive,
+    color,
+    size,
+  });
+}
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) =>
+            getTabBarIcon(route.name, focused, color, size),
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })}>
+        <Tab.Screen name="HomeTab" component={HomeTabStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeTabStack = createNativeStackNavigator();
+function HomeTabStackScreen() {
+  return (
+    <HomeTabStack.Navigator initialRouteName="HomeScreen">
+      <HomeTabStack.Screen name="HomeScreen" component={HomeScreen} />
+    </HomeTabStack.Navigator>
+  );
+}
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
       <Text style={styles.titleText}>React Native</Text>
       <StatusBar barStyle="light-content" />
       <Counter />
-    </SafeAreaView>
+    </View>
   );
 };
 
