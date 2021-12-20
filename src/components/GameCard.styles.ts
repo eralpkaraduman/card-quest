a396ffe5ec01fd1adb970f2347cb410b516ccb07
Eralp Karaduman <eralp@eralpkaraduman.com>
2021-12-21 00:24:42 +0200

    Refactor styles

---
+++ src/components/GameCard.styles.ts
@@ -0,0 +1,80 @@
+import styled, {css} from 'styled-components/native';
+import {DonsolCardKind} from '@controllers/DonsolCard';
+import WebIcon from '@/icons.web';
+
+const suitIconSize = 17;
+const WHRatio: number = 2.5 / 3.5;
+
+export enum GameCardSize {
+  medium = 120,
+  large = 190,
+}
+
+export const MaterialSuitIcon = styled(WebIcon.MaterialCommunityIcon).attrs(
+  ({theme, color}) => ({
+    size: suitIconSize,
+    color: theme.colors[color],
+  }),
+)``;
+
+export const FontAwesomeSuitIcon = styled(WebIcon.FontAwesome5Icon).attrs(
+  ({theme, color}) => ({
+    size: suitIconSize,
+    solid: true,
+    color: theme.colors[color],
+  }),
+)``;
+
+export const CardKindIcon = styled(WebIcon.MaterialCommunityIcon).attrs(
+  ({theme, kind}) => ({
+    size: 30,
+    color: theme.colors[kind],
+  }),
+)<{kind: DonsolCardKind}>``;
+
+export const Container = styled.View<{size: GameCardSize}>`
+  ${({size, theme}) => css`
+    display: flex;
+    border-radius: ${size / 20}px;
+    border-color: ${theme.colors.gray};
+    border-width: 2px;
+    border-style: solid;
+    width: ${size * WHRatio}px;
+    height: ${size}px;
+    padding: ${theme.dimensions.padding.xsmall}px;
+  `};
+`;
+
+export const CardEffectValueText = styled.Text<{
+  kind: DonsolCardKind;
+}>`
+  color: ${({theme, kind}) => theme.colors[kind]};
+  font-size: 17px;
+  font-weight: bold;
+`;
+
+export const CardContentContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  gap: ${({theme}) => theme.dimensions.padding.xsmall}px;
+  padding-bottom: ${({theme}) => theme.dimensions.padding.small}px;
+`;
+
+export const CardHeader = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: ${({theme}) => theme.dimensions.padding.small}px;
+  padding-left: ${({theme}) => theme.dimensions.padding.small}px;
+  padding-top: ${({theme}) => theme.dimensions.padding.small}px;
+`;
+
+export const ValueText = styled.Text`
+  color: ${({theme}) => theme.colors.secondary};
+  font-size: 21px;
+  font-weight: bold;
+  margin-left: ${({theme}) => theme.dimensions.padding.xsmall}px;
+`;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,66 +1,26 @@
 import React, {ReactElement} from 'react';
-import {View} from 'react-native';
-import styled, {css} from 'styled-components/native';
 import {DonsolCard, DonsolCardKind} from '@controllers/DonsolCard';
-import WebIcon from '@/icons.web';
 import {CardSuit} from '@controllers/Deck';
+import * as Styles from './GameCard.styles';
 
-const WHRatio: number = 2.5 / 3.5;
-
-export enum GameCardSize {
-  medium = 120,
-  large = 190,
-}
-
-const Container = styled.View<{size: GameCardSize}>`
-  ${({size, theme}) => css`
-    display: flex;
-    border-radius: ${size / 20}px;
-    border-color: ${theme.colors.gray};
-    border-width: 2px;
-    border-style: solid;
-    width: ${size * WHRatio}px;
-    height: ${size}px;
-    padding: ${theme.dimensions.padding.xsmall}px;
-  `};
-`;
+export {GameCardSize} from './GameCard.styles';
 
 export interface GameCard_Props {
   donsolCard: DonsolCard;
-  size: GameCardSize;
+  size: Styles.GameCardSize;
 }
 
-const suitIconSize = 17;
-
-const MaterialSuitIcon = styled(WebIcon.MaterialCommunityIcon).attrs(
-  ({theme, color}) => ({
-    size: suitIconSize,
-    color: theme.colors[color],
-  }),
-)``;
-
-const FontAwesomeSuitIcon = styled(WebIcon.FontAwesome5Icon).attrs(
-  ({theme, color}) => ({
-    size: suitIconSize,
-    solid: true,
-    color: theme.colors[color],
-  }),
-)``;
-
-const CardKindIcon = styled(WebIcon.MaterialCommunityIcon).attrs(
-  ({theme, kind}) => ({
-    size: 30,
-    color: theme.colors[kind],
-  }),
-)<{kind: DonsolCardKind}>``;
-
 const CardSuitIconMap: {[key in CardSuit]: ReactElement} = {
-  [CardSuit.clubs]: <MaterialSuitIcon name="cards-club" color="white" />,
-  [CardSuit.spades]: <MaterialSuitIcon name="cards-spade" color="white" />,
-  [CardSuit.hearts]: <MaterialSuitIcon name="cards-heart" color="red" />,
-  [CardSuit.diamonds]: <MaterialSuitIcon name="cards-diamond" color="red" />,
+  [CardSuit.clubs]: <Styles.MaterialSuitIcon name="cards-club" color="white" />,
+  [CardSuit.spades]: (
+    <Styles.MaterialSuitIcon name="cards-spade" color="white" />
+  ),
+  [CardSuit.hearts]: <Styles.MaterialSuitIcon name="cards-heart" color="red" />,
+  [CardSuit.diamonds]: (
+    <Styles.MaterialSuitIcon name="cards-diamond" color="red" />
+  ),
   [CardSuit.joker]: (
-    <FontAwesomeSuitIcon name="kiss-wink-heart" color="yellow" />
+    <Styles.FontAwesomeSuitIcon name="kiss-wink-heart" color="yellow" />
   ),
 };
 
@@ -70,56 +30,25 @@ const CardKindIconNameMap: {[key in DonsolCardKind]: string} = {
   [DonsolCardKind.shield]: 'shield',
 };
 
-const CardEffectValueText = styled.Text<{
-  kind: DonsolCardKind;
-}>`
-  color: ${({theme, kind}) => theme.colors[kind]};
-  font-size: 17px;
-  font-weight: bold;
-`;
-
-const CardContentContainer = styled.View`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  gap: ${({theme}) => theme.dimensions.padding.xsmall}px;
-`;
-
-const CardHeader = styled(View)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding-top: ${({theme}) => theme.dimensions.padding.small}px;
-  padding-left: ${({theme}) => theme.dimensions.padding.small}px;
-  padding-top: ${({theme}) => theme.dimensions.padding.small}px;
-`;
-
-const ValueText = styled.Text`
-  color: ${({theme}) => theme.colors.secondary};
-  font-size: 21px;
-  font-weight: bold;
-  margin-left: ${({theme}) => theme.dimensions.padding.xsmall}px;
-`;
-
 export function GameCard({donsolCard, size}: GameCard_Props): ReactElement {
   const {suit, value} = donsolCard.card;
   return (
-    <Container size={size}>
-      <CardHeader>
+    <Styles.Container size={size}>
+      <Styles.CardHeader>
         {CardSuitIconMap[suit]}
-        {suit !== CardSuit.joker && <ValueText>{value}</ValueText>}
-      </CardHeader>
-      <CardContentContainer>
-        <CardKindIcon
+        {suit !== CardSuit.joker && (
+          <Styles.ValueText>{value}</Styles.ValueText>
+        )}
+      </Styles.CardHeader>
+      <Styles.CardContentContainer>
+        <Styles.CardKindIcon
           kind={donsolCard.kind}
           name={CardKindIconNameMap[donsolCard.kind]}
         />
-        <CardEffectValueText kind={donsolCard.kind}>
+        <Styles.CardEffectValueText kind={donsolCard.kind}>
           {donsolCard.effect}
-        </CardEffectValueText>
-      </CardContentContainer>
-    </Container>
+        </Styles.CardEffectValueText>
+      </Styles.CardContentContainer>
+    </Styles.Container>
   );
 }
