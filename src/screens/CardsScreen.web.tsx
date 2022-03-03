7e051728a2a8615743d59654022739c10de34e83
Eralp Karaduman <eralp@eralpkaraduman.com>
2022-03-03 03:24:39 +0200

    Minor layout fix

--- src/screens/CardsScreen.web.tsx
+++ src/screens/CardsScreen.web.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import {TitleText} from '@components/TitleText';
 import {Helmet} from 'react-helmet';
 import {CardList} from '@components/CardList';
+import styled from 'styled-components/native';
+
+const PageTitleText = styled(TitleText)`
+  margin-bottom: ${({theme}) => theme.dimensions.padding.medium};
+`;
 
 export function CardsScreen(): React.ReactElement {
   return (
     <>
       <Helmet>
-        <title>Card Quest: Cards</title>
+        <title>Card Quest: All Cards</title>
       </Helmet>
-      <TitleText>Cards</TitleText>
+      <PageTitleText>All Cards</PageTitleText>
       <CardList />
     </>
   );
