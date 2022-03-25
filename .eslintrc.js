c24a1454ef3d4bb48ffed79fe7c5d79bc63f5cab
Eralp Karaduman <eralp@eralpkaraduman.com>
2022-03-25 18:30:25 +0200

    Update ts and linter config to allow DOM types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
